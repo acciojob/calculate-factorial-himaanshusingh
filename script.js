640377ee02ec8cc82a6d9e9ec470c76e88df73aa
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-09 10:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const value = parseInt(prompt("Enter a number to find the factorial"));
+
+function fact(n) {
+  if (n <= 1) return 1;
+  return n * fact(n - 1);
+}
+
+alert(`The factorial of ${value} is ${fact(value)}`);
